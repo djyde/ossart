906459ac1a15253700054bc5838f570e58fbdf7a
Randy <randypriv@gmail.com>
2022-03-18 22:33:51 +0800

    cache

---
+++ cache.ts
@@ -0,0 +1,32 @@
+import { Low, JSONFile, JSONFileSync } from "lowdb";
+import path from 'path'
+import fs from 'fs'
+export class Cache {
+
+  private cachePath = path.join(process.cwd(), 'cache.json')
+  private adapter = new JSONFile(this.cachePath)
+  private db = new Low(this.adapter)
+
+  constructor(private cacheName: string) {
+  }
+
+  async get(key: string) {
+    await this.db.read()
+    if (!this.db.data[this.cacheName]) {
+      return null
+    }
+    return this.db.data[this.cacheName][key];
+  }
+
+  async set(key: string, value: any) {
+    await this.db.read()
+    if (!this.db.data[this.cacheName]) {
+      this.db.data[this.cacheName] = {}
+      await this.db.write()
+      console.log('write')
+    }
+    await this.db.read()
+    this.db.data[this.cacheName][key] = value
+    await this.db.write()
+  }
+}
--- core.ts
+++ core.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import dayjs from "dayjs";
+import { Cache } from "./cache";
 import { apiClient } from "./utils.client";
 var utc = require("dayjs/plugin/utc");
 dayjs.extend(utc);
@@ -47,14 +48,23 @@ export async function getYearCalendar(params: {
 }
 
 export async function getFiveYearsCalendar(params: { login: string }) {
+  const cache = new Cache("five_years");
+  if (await cache.get(params.login)) {
+    console.log('use cache')
+    return await cache.get(params.login);
+  }
   const cal = await Promise.all(
     [0, 0, 0, 0, 0, 0].map(async (_, index) => {
-      const year = dayjs().subtract(index + 1, "year").get("year").toString();
+      const year = dayjs()
+        .subtract(index + 1, "year")
+        .get("year")
+        .toString();
       return await getYearCalendar({
         login: params.login,
         fromYear: year,
       });
     })
   );
-  return cal
+  cache.set(params.login, cal);
+  return cal;
 }
